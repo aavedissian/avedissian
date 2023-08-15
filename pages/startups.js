81cdc4a4c3b5ab8c860265f738818ef7c917dadf
Anthony Avedissian <33400917+aavedissian@users.noreply.github.com>
2023-08-15 00:12:09 -0400

    Update startups.js

--- pages/startups.js
+++ pages/startups.js
@@ -5,7 +5,7 @@ export default function Startups() {
     <Layout>
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>Startups</h2>
-          <p>I invest in Pre-Seed, Seed, and Series A rounds and love meeting founders, playing with <a target="_blank" href="https://cdixon.org/2010/01/03/the-next-big-thing-will-start-out-looking-like-a-toy">toys</a>, and navigating the <a target="_blank" href="https://cdixon.org/2013/08/04/the-idea-maze">idea maze</a>. If you are seeking investment, product feedback, or strategic advice, please email me at <a href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a>.</p>
+          <p>I invest in angel, pre-seed, and seed rounds. I love meeting founders, playing with <a target="_blank" href="https://cdixon.org/2010/01/03/the-next-big-thing-will-start-out-looking-like-a-toy">toys</a>, and navigating the <a target="_blank" href="https://cdixon.org/2013/08/04/the-idea-maze">idea maze</a>. If you are seeking investment, product feedback, or strategic advice, please email me at <a href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a>.</p>
           <p>To streamline the process, feel free to include a Google Doc with answers to the following:</p>
           <p>What’s your company’s purpose?</p>
           <p>What problem are you solving?</p>
