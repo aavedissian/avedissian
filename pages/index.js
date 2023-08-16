dd2c538786650c3a6fd6cf9f03f9e7a8580d6a54
aavedissian <avedissian9@gmail.com>
2023-08-15 18:58:52 -0700

    w2

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>About</h2>
           <Image src='/aa.jpg' width={150} height={150} alt='author' className='rounded-full' />
-          <p>My name is Anthony Avedissian. I live in Los Angeles with my wife and invest in technology startups at <a className='text-blue-600' target='_blank' href="https://seedclub.ventures/">Seed Club Ventures</a>.</p>
+          <p>My name is Anthony Avedissian. I live in Los Angeles and invest in technology startups at <a className='text-blue-600' target='_blank' href="https://seedclub.ventures/">Seed Club Ventures</a>.</p>
           <p>I started my career at <a className='text-blue-600' target='_blank' href="https://www.ambercapital.com/">Amber Capital</a>, an activist hedge fund, and later worked in investment banking at <a className='text-blue-600' target='_blank' href="https://www.sc.com/en/">Standard Chartered</a>.</p>
           <p>I grew up in London, regularly traveling to Istanbul and Yerevan. I have lived in 10+ cities, including Miami, Lisbon, Shanghai, Beirut, Phuket, Dubai, and Barcelona.</p>
           <p>I enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, running, swimming, skiing, hiking, and training Muay Thai.</p>
