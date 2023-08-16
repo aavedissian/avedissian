08c1598bc477aa25595421274743d04f3f100e0d
aavedissian <avedissian9@gmail.com>
2023-08-15 18:39:09 -0700

    act

--- pages/principles.js
+++ pages/principles.js
@@ -9,7 +9,7 @@ export default function Books() {
           <ul class="list-disc list-inside"> 
           <li><span className="font-bold">Transcend Paradigms.</span> Mental models are <a className='text-blue-600' target="_blank" href="https://fs.blog/map-and-territory/">useful abstractions</a>, but not the same as reality. Remain flexible and intellectually unattached.</li>
 
-          <li><span className="font-bold">Take Action.</span> In the <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Buridan%27s_ass">Buridan’s Ass</a>, the donkey dies because it is unable to decide. Do not be the donkey. Take action, even when no option is preferable.</li>
+          <li><span className="font-bold">Take Action.</span> In the <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Buridan%27s_ass">Buridan’s Ass</a>, the donkey dies because it is unable to decide. Do not be the donkey. Act, even when no option is preferable.</li>
           
           <li><span className="font-bold">Compound.</span> The greatest returns in life come from <a className='text-blue-600' href="https://anthonyavedissian.substack.com/p/on-compounding">compounding</a>. Keep consistent and play the long game, especially for relationships.</li>
           
