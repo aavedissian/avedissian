ea8970330a6c4c74d8c03a590a52db077b15ee64
aavedissian <avedissian9@gmail.com>
2023-08-16 10:23:42 -0700

    text

--- pages/armenia.js
+++ pages/armenia.js
@@ -5,9 +5,13 @@ export default function Books() {
     <Layout>
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>Armenia</h2>
+   
           <p>I have lived in Yerevan for extended periods and toured much of Armenia and its neighbouring regions, including Georgia, Artsakh, and Anatolia.</p>
+ 
           <p>I won <a className='text-blue-600' target="_blank" href="https://neruzh.am/en/">Neruzh</a> in 2018 and participated in the <a className='text-blue-600' target="_blank" href="https://fast.foundation/">FAST accelerator</a> in 2019. I am excited about the emerging technology and startup ecosystem in Armenia.</p>
+  
           <p>I am interested in investing in Armenian startups and supporting local initiatives in education, healthcare, and pharmaceuticals.</p>
+  
           <p>Please message me on <a className='text-blue-600' target="_blank" href="https://twitter.com/antavedissian">Twitter</a> or email <a className='text-blue-600' href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a> to discuss further.</p>
         </div>
     </Layout>
--- pages/startups.js
+++ pages/startups.js
@@ -5,17 +5,29 @@ export default function Startups() {
     <Layout>
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>Startups</h2>
+    
           <p>I invest in pre-seed, seed, and series A rounds. I love meeting founders, playing with <a className='text-blue-600' target="_blank" href="https://cdixon.org/2010/01/03/the-next-big-thing-will-start-out-looking-like-a-toy">toys</a>, and navigating the <a className='text-blue-600' target="_blank" href="https://cdixon.org/2013/08/04/the-idea-maze">idea maze</a>.</p>
+    
           <p>I help startups with business development, product strategy, and introductions to customers and investors.</p>
+    
           <p>If you are seeking investment, product feedback, or strategic advice, please message me on <a className='text-blue-600' target="_blank" href="https://twitter.com/antavedissian">Twitter</a> or email <a className='text-blue-600' href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a>.</p>
+    
           <p>To streamline the process, feel free to include a Google Doc answering:</p>
+    
           <ul class="list-disc list-inside"> 
+    
           <li>What is the mission of your company?</li>
+    
           <li>What problem is your company solving?</li>
+    
           <li>What is your solution to the problem?</li>
+    
           <li>Why build the company now?</li>
+    
           <li>How do you plan to make money?</li>
+    
           <li>Who is on the team?</li>
+    
           <li>What progress have you made?</li>
           </ul>
         </div>
