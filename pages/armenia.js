e5e87b145c6990bf6e7e3aa326162f0ed6876bee
aavedissian <avedissian9@gmail.com>
2023-08-15 17:00:11 -0700

    many more changes

--- pages/armenia.js
+++ pages/armenia.js
@@ -6,9 +6,9 @@ export default function Books() {
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>Armenia</h2>
           <p>I have lived in Yerevan for extended periods and toured much of Armenia and its neighbouring regions, including Georgia, Artsakh, and Anatolia.</p>
-          <p>I won <a target="_blank" href="https://neruzh.am/en/">Neruzh</a> in 2018 and participated in the <a target="_blank" href="https://fast.foundation/">FAST accelerator</a> in 2019. I am excited about the emerging technology and startup ecosystem in Armenia.</p>
+          <p>I won <a className='text-blue-600' target="_blank" href="https://neruzh.am/en/">Neruzh</a> in 2018 and participated in the <a className='text-blue-600' target="_blank" href="https://fast.foundation/">FAST accelerator</a> in 2019. I am excited about the emerging technology and startup ecosystem in Armenia.</p>
           <p>I am interested in investing in Armenian startups and supporting local initiatives in education, healthcare, and pharmaceuticals.</p>
-          <p>Message me on <a target="_blank" href="https://twitter.com/antavedissian">Twitter</a> or email <a href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a> to discuss further.</p>
+          <p>Please message me on <a className='text-blue-600' target="_blank" href="https://twitter.com/antavedissian">Twitter</a> or email <a className='text-blue-600' href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a> to discuss further.</p>
         </div>
     </Layout>
   )
--- pages/startups.js
+++ pages/startups.js
@@ -5,9 +5,9 @@ export default function Startups() {
     <Layout>
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>Startups</h2>
-          <p>I invest in Pre-Seed and Seed rounds. I love meeting founders, playing with <a target="_blank" href="https://cdixon.org/2010/01/03/the-next-big-thing-will-start-out-looking-like-a-toy">toys</a>, and navigating the <a target="_blank" href="https://cdixon.org/2013/08/04/the-idea-maze">idea maze</a>.</p>
+          <p>I invest in Pre-Seed and Seed rounds. I love meeting founders, playing with <a className='text-blue-600' target="_blank" href="https://cdixon.org/2010/01/03/the-next-big-thing-will-start-out-looking-like-a-toy">toys</a>, and navigating the <a className='text-blue-600' target="_blank" href="https://cdixon.org/2013/08/04/the-idea-maze">idea maze</a>.</p>
           <p>I help startups with business development, product strategy, and introductions to customers and investors.</p>
-          <p>If you are seeking investment, product feedback, or strategic advice, please email <a href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a>.</p>
+          <p>If you are seeking investment, product feedback, or strategic advice, please email <a className='text-blue-600' href="mailto:anthony@seedclub.ventures">anthony@seedclub.ventures</a>.</p>
           <p>To streamline the process, feel free to include a Google Doc with answers to the following:</p>
           <ul class="list-disc list-inside"> 
           <li>What is the mission of your company?</li>
