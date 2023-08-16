27b94fec8c93ec6315e88d5745737821e65bd418
aavedissian <avedissian9@gmail.com>
2023-08-15 17:39:57 -0700

    final principles

--- pages/principles.js
+++ pages/principles.js
@@ -5,31 +5,27 @@ export default function Books() {
     <Layout>
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>Principles</h2>
-          <p>Principles I regularly come back to.</p>
+          <p>Some principles I regularly come back to.</p>
           <ul class="list-disc list-inside"> 
-          <li> Transcend Paradigms. Mental models are useful abstractions, but they are not the same as reality. Remain intellectually unattached and flexible – our universe is immense, amazing, and far beyond human comprehension.</li>
+          <li><span className="font-bold">Transcend Paradigms.</span> Mental models are <a className='text-blue-600' target="_blank" href="https://fs.blog/map-and-territory/">useful abstractions</a>, but are not the same as reality. Remain intellectually unattached and flexible.</li>
           
-          <li>Take Action. In the <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Buridan%27s_ass">Buridan’s Ass</a>, the donkey dies because it is unable to decide. Do not be the donkey. Take action, even when no option is preferable.</li>
+
+
+          <li><span className="font-bold">Take Action.</span> In the <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Buridan%27s_ass">Buridan’s Ass</a>, the donkey dies because it is unable to decide. Do not be the donkey. Take action, even when no option is preferable.</li>
           
-          <li>Compound. The greatest returns in life, whether in wealth, relationships, or knowledge, come from <a className='text-blue-600' href="https://anthonyavedissian.substack.com/p/on-compounding">compound interest</a>.</li>
+          <li><span className="font-bold">Compound.</span> The greatest returns in life come from <a className='text-blue-600' href="https://anthonyavedissian.substack.com/p/on-compounding">compound interest</a>.Keep consistent and play the long game, especially for relationships.</li>
           
-          <li>Prioritize. Decide on your most important priority for this: decade, year, quarter, and month. Systematically allocate time to meet those priorities.</li>
-          
-          <li>Engineer serendipity. Luck is not magic. It is a state of mind and a property of networks. It can be measured, analyzed, and <a className='text-blue-600' href="https://medium.com/aspen-ideas/engineering-serendipity-941e601a9b65">engineered</a>.</li>
-          
-          <li>Invert. To be successful, rather than study success, which is subject to survivorship bias, study the causes of failure and engineer the elimination or mitigation of those risks.</li>
+          <li><span className="font-bold">Prioritize.</span> Decide on your most important priority for this: decade, year, quarter, and month. Systematically allocate time to meet those priorities.</li>
                     
-          <li>Remain curious. Stay as close to the edge as you can without going over. You will discover new things unseen from the center.</li>
-          
-          <li>Live holistically. Work-life balance is a <a className='text-blue-600' target="_blank" href="https://blog.nateliason.com/p/work-life-balance">false dichotomy</a>, implying work and life must be traded off. Instead, integrate work as one part of a holistic life, where each area supports and complements others.</li>
-          
-          <li>Radical honesty. Honesty, not only with others but also oneself, leads to deeper relationships, clear decision-making, and a more authentic life.</li>
+          <li><span className="font-bold">Invert.</span> To be successful, rather than study success, <a className='text-blue-600' href="https://jamesclear.com/inversion">study the causes of failure</a> and engineer the elimination or mitigation of those risks.</li>
                               
-          <li>Look forward. Make decisions based on <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Sunk_cost">future value</a>, not unrecoverable past events/investments.</li>
+          <li><span className="font-bold">Live holistically.</span> Work-life balance is a <a className='text-blue-600' target="_blank" href="https://blog.nateliason.com/p/work-life-balance">false dichotomy</a>. Integrate work as one part of a holistic life, where each area supports and complements others.</li>
+          
+          <li><span className="font-bold">Radical honesty.</span> Honesty, not only with others but also oneself, leads to deeper relationships, clear decision-making, and a more authentic life.</li>
+                              
+          <li><span className="font-bold">Look forward.</span> Make decisions based on <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Sunk_cost">future value</a>, not unrecoverable past investments.</li>
 
-          <li>Simplify. You do not understand something well enough until you can explain it simply. In a few sentences, with simple words.</li>
-
-          <li>Maintain efficiency. 80% of results are often driven by <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Pareto_principle">20% of efforts</a>.</li>
+          <li><span className="font-bold">Maintain efficiency.</span> 80% of results are often driven by <a className='text-blue-600' target="_blank" href="https://en.wikipedia.org/wiki/Pareto_principle">20% of efforts</a>.</li>
 
           </ul>
         </div>
