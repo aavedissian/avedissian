26db5c5d6ab8c3ce1cc43ff7ba53455c432ab562
aavedissian <avedissian9@gmail.com>
2024-02-15 11:16:12 -0500

    teavel

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
      
           <p>I live in Los Angeles with my wife and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, hiking, and Muay Thai.</p> 
 
-          <p>I grew up in London, regularly traveling to Istanbul and Yerevan, and have lived in 10+ cities, including Miami, Lisbon, Shanghai, Phuket, and Barcelona.</p>
+          <p>I grew up in London and have lived in Istanbul, Yerevan, Miami, Lisbon, Shanghai, Phuket, and Barcelona.</p>
 
           <p>You can reach me on <a className='text-blue-600' target='_blank' href="https://twitter.com/antavedissian">Twitter</a> and find my writing on <a className='text-blue-600' target='_blank' href="https://anthonyavedissian.substack.com/">Substack</a>.</p>
         </div>
