95bad65cfa1d4465a0dccaddf3e8e4e07ef65ea2
aavedissian <avedissian9@gmail.com>
2024-05-15 14:25:56 -0700

    tennis

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           <Image src='/aa.jpg' width={150} height={150} alt='author' className='rounded-full' />
           <p>My name is Anthony Avedissian. I invest in startups at <a className='text-blue-600' target='_blank' href="https://seedclub.ventures/">Seed Club Ventures</a>.</p>
      
-          <p>I live in Los Angeles with my wife and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, tennis, hiking, and Muay Thai.</p> 
+          <p>I live in Los Angeles with my wife and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, hiking, tennis, and Muay Thai.</p> 
 
           <p>I grew up in London and have lived in Istanbul, Yerevan, Miami, Lisbon, Shanghai, Phuket, and Barcelona.</p>
 
