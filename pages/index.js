4f90d66ae959a590b3bf63098c96ec34d4d3e7be
aavedissian <avedissian9@gmail.com>
2024-05-25 11:21:15 -0700

    pump

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
         <div className='flex flex-col gap-4'>
           <h2 className='font-bold text-4xl'>About</h2>
           <Image src='/aa.jpg' width={150} height={150} alt='author' className='rounded-full' />
-          <p>My name is Anthony Avedissian. I invest in early-stage startups at <a className='text-blue-600' target='_blank' href="https://www.seedclub.xyz/">Seed Club</a>.</p>
+          <p>My name is Anthony Avedissian. I invest in early-stage startups at <a className='text-blue-600' target='_blank' href="https://seedclub.ventures/">Seed Club Ventures</a>. Together with my wife, Lara, I also design and craft <a className='text-blue-600' target='_blank' href="https://www.laravalentina.com/">fine jewelry</a>.</p>
      
-          <p>I live in Los Angeles with my wife and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, hiking, tennis, and Muay Thai.</p> 
+          <p>I live in Los Angeles and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, golf, tennis, and Muay Thai.</p> 
 
           <p>I grew up in London and have lived in Istanbul, Yerevan, Miami, Lisbon, Shanghai, Phuket, and Barcelona.</p>
 
