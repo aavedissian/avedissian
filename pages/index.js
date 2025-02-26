12016734f5d9feb48efb1a4ef221acf996330dc7
aavedissian <avedissian9@gmail.com>
2025-02-25 23:04:38 -0800

    joel

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           <Image src='/aa.jpg' width={150} height={150} alt='author' className='rounded-full' />
           <p>My name is Anthony Avedissian. I invest in and advise early-stage startups. Together with my wife, Lara, I also design and craft <a className='text-blue-600' target='_blank' href="https://www.laravalentina.com/">fine jewelry</a>.</p>
      
-          <p>I live in Los Angeles and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, golf, tennis, and Muay Thai.</p> 
+          <p>I live in Los Angeles and enjoy reading, <a className='text-blue-600' target='_blank' href="https://www.instagram.com/antilosh.eats/">cooking</a>, swimming, skiing, hiking, golf, tennis, and Muay Thai.</p> 
 
           <p>I grew up in London and have lived in Istanbul, Yerevan, Miami, Lisbon, Shanghai, Phuket, and Barcelona.</p>
 
